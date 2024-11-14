b946615f5723dd971cee6bedd8930b8f9cd22c62
Tomas Gutierrez <tomasgp1997@gmail.com>
2024-11-14 11:32:09 -0300

    ProductSeeder

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,10 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        DB::table('products')->insert([
+            'name' => 'Producto 1',
+            'description' => 'Descripción del producto 1',
+            'price' => 10.99,
+        ],
+        [
+            'name' => 'Producto 2',
+            'description' => 'Descripción del producto 2',
+            'price' => 19.99,
+        ],
+        [
+            'name' => 'Producto 3',
+            'description' => 'Descripción del producto 3',
+            'price' => 29.99,
+        ]
+        );
+
+
     }
 }
